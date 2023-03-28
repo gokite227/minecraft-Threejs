b41559d5f4938a604f737e2ab6ee4e8daca91441
gokite227 <102020249+gokite227@users.noreply.github.com>
2023-03-28 22:16:47 +0900

    player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Canvas } from '@react-three/fiber';
+import { Sky } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import { Ground } from './components/Ground';
+import { Player } from './components/Player';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
       <Canvas>
+        <Sky sunPosition={[100,100,20]}/>
+        <ambientLight intensity={0.5} /> {/*물체에 적용되는 광원의 밝기*/}
+        <Physics> 
+          <Player />
+          <Ground />
+        </Physics>
       </Canvas>
     </>
   );
